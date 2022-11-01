15f3090cb6d3a81a85c6fcbf5fc88aa9a994656c
newfrogg <85973867+newfrogg@users.noreply.github.com>
2022-11-01 15:18:46 +0700

    practice example: key press

---
+++ STM32/Lab 3/Example/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 1, 2022
+ *      Author: Beelzebufo
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer_flag;
+void setTimer(int duration);
+
+void runTimer();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
